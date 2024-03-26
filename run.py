3d445641d75c0270392f4be4af87e1ebee1d07a9
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-03-26 13:05:58 +0000

    calculate surplus data

--- run.py
+++ run.py
@@ -74,7 +74,7 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully \n")
 
 
-def calculate_surplus_data(data):
+def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculates the surplus for each item type.
     Stock value minus Sales sales value equals to surplus
@@ -84,8 +84,12 @@ def calculate_surplus_data(data):
     print("Calculating surplus data ..... \n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
-
+    surplus_data = []
+    for stock , sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 def main():
     
@@ -93,9 +97,10 @@ def main():
     Run all the profram function. 
     """
     data = get_sales_data()
-    sales_data = [num for num in data]
+    sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 
